a5348eaa0d1160848b3d334e7c3c5bb381509a74
Elver <holahacker01@gmail.com>
2023-11-22 12:22:18 -0500

    Initial Commit

---
+++ WebControlApp/WebControlAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebControlAppApp.swift
+//  WebControlApp
+//
+//  Created by Elver Mayta Hernández on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WebControlAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
